c9c9aedf616a5e3403ac50c892f9ad6733c6412c
Antonis Papadakis <antonispap95@gmail.com>
2020-11-28 20:46:22 +0200

    first commit

---
+++ graph.py
@@ -0,0 +1,50 @@
+import os
+import networkx as nx
+import community as community_louvain
+from networkx.algorithms import bipartite
+from networkx.algorithms import community
+
+
+def louvain(P, res=1.0, seed=None):
+    partition = community_louvain.best_partition(
+        P, resolution=res, random_state=seed)
+    communities = {k: v for k, v in sorted(
+        partition.items(), key=lambda item: item[1])}
+    return communities
+
+
+def partition(part_dict):
+    part_list = list()
+    for i in range(max(part_dict.values())+1):
+        part_list.append([])
+    for i, j in part_dict.items():
+        part_list[j].append(i)
+    return part_list
+
+
+def write(l):
+    with open('C:/Users/anton/Desktop/evaluation/clusters.txt', mode='w') as f:
+        for line in partition(l):
+            f.write(' '.join(line) + '\n')
+
+
+def evaluate(l):
+    part = partition(l)
+    metrics = {'Communities': max(l.values())+1,
+               'Modularity': community.modularity(P, part),
+               'Coverage': community.coverage(P, part),
+               'Performance': community.performance(P, part)
+               }
+    return metrics
+
+
+G = nx.read_edgelist('new.edges', delimiter=',')
+
+politicians = bipartite.sets(G)[1]
+
+P = bipartite.overlap_weighted_projected_graph(G, politicians, jaccard=False)
+
+louv = louvain(P, 0.95, 5)
+
+write(louv)
+print(evaluate(louv))
